149a3b20ab86f7fd89b2d6c734aa4f91bf556cfb
AndreaOffredi <info@andreaoffredi.com>
2024-06-29 16:04:54 +0200

    ADD: Esercizio 2 e 2B

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 // Esercizio 1
-let numeber = 100
-console.log(Number)
+let number = 100
+console.log(number)
+
+// Esercizio 2
+const PI = 3.14
+console.log(PI)
+
+// Esercizio 2B per verificare errore
+PI = 3.1416
+
+// Esercizio 3
+
