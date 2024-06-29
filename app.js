f23360de0fc1c035dd6ace2e4c0b897a1c7fa401
AndreaOffredi <info@andreaoffredi.com>
2024-06-29 16:09:45 +0200

    ADD: commenti accanto ai valore delle variabili

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 // Esercizio 1
-let number = 100
+let number = 100 //number
 console.log(number)
 
 // Esercizio 2
-const PI = 3.14
+const PI = 3.14 //number
 console.log(PI)
 
 // Esercizio 2B per verificare errore
 PI = 3.1416
 
 // Esercizio 2C
-let radius = 8
+let radius = 8 //number
 let circle = radius * 2 * PI
 console.log(circle)
 
 // Esercizio 3
-let name = 'Carlo'
-name = 'Marco'
+let name = 'Carlo' //string
+name = 'Marco' //string
 console.log(name)
 
 
